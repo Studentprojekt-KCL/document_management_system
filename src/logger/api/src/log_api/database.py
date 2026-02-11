3c62541d72c24a9adc8428e85d304c53bf20f575
Alexander Järnström <aljr21@student.bth.se>
2026-02-11 22:03:37 +0100

    added type validation and support to grab from database #72

--- src/logger/api/src/log_api/database.py
+++ src/logger/api/src/log_api/database.py
@@ -1,7 +1,9 @@
 """Copyright (c) 2026, Studentprojekt Knowit Cybersecurity and Law"""
 
 import os
+from datetime import datetime
 import mysql.connector
+from mysql.connector.types import RowItemType, RowType
 from .models import Log
 
 
@@ -15,16 +17,46 @@ def connect():
 
     return mysql.connector.connect(host=host, user=user, database=database, password=password)
 
+def extract_row_data(row: RowType | dict[str, RowItemType]) -> Log:
+    """Extract each values from a row validating the type.
+    
+    Keyword arguments:
+    row -- Row from the database.
+    """
 
-def database_get_logs():
+    if not isinstance(row, tuple):
+        raise TypeError("Row is of wrong type.")
+
+    id: int | None = row[0] if isinstance(row[0], int) else None
+    occured: datetime | None = row[1] if isinstance(row[1], datetime) else None
+    message: str | None = row[2] if isinstance(row[2], str) else None
+    event_type: str | None = row[3] if isinstance(row[3], str) else None
+    service: str | None = row[4] if isinstance(row[4], str) else None
+
+    if id is None:
+        raise TypeError("Id is of wrong type.")
+    if occured is None:
+        raise TypeError("Occured is of wrong type.")
+    if message is None:
+        raise TypeError("Message is of wrong type.")
+    if event_type is None:
+        raise TypeError("Event Type is of wrong type.")
+    if service is None:
+        raise TypeError("Service is of wrong type.")
+
+    return Log(id = id, occured = occured, message = message, event_type = event_type, service = service)
+
+
+def database_get_logs(start: datetime, end: datetime) -> list[Log]:
     """Grab all logs."""
 
     db = connect()
     cursor = db.cursor()
-    _ = cursor.execute("SELECT * FROM logs")
-    results = cursor.fetchall()
-    for result in results:
-        print(result)
+    query: str = "SELECT * FROM logs WHERE occured BETWEEN %s AND %s"
+    _ = cursor.execute(query, (start, end))
+    result = cursor.fetchall()
+
+    return [extract_row_data(row) for row in result]
 
 
 def database_add_log(log: Log) -> Log:

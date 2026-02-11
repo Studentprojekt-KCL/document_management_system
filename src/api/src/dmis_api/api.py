00c185d0c1e82484259905a6b30863d4ffeed3cb
jaen22BTH <jaen22@student.bth.se>
2026-02-11 11:32:29 -0500

    Migrated to uvicorn initiation (ref #84)

--- src/api/src/dmis_api/api.py
+++ src/api/src/dmis_api/api.py
@@ -1,8 +1,10 @@
 """Copyright (c) 2026, Studentprojekt Knowit Cybersecurity and Law."""
 
 import sys
-
 from typing import Any
+import argparse
+
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
@@ -12,12 +14,13 @@ from dmis_api.structures import IndexRequest
 
 app = FastAPI()
 
+log_level: str | None = None
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(_: Request, exc: RequestValidationError) -> JSONResponse:
     """Overwrite FastAPI exception handeler."""
     content: str | dict
-    if sys.argv[1] == "dev":
+    if log_level == "debug":
         content = jsonable_encoder({"detail": exc.errors(), "body": exc.body})
     else:
         content = "ERROR"
@@ -28,3 +31,12 @@ async def validation_exception_handler(_: Request, exc: RequestValidationError) 
 async def index(item: IndexRequest) -> Any:
     """DMIS API index endpoint definition."""
     return item
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dev", action="store_true")
+    args = parser.parse_args()
+    if args.dev:
+        global log_level
+        log_level = "debug"
+    uvicorn.run(app, host="0.0.0.0", log_level=log_level)

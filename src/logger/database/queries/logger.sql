1d6ab08f2db50250685b4d891c244866b937a298
Alexander Järnström <aljr21@student.bth.se>
2026-02-11 14:46:25 +0000

    added primary key `id` #80

--- src/logger/database/queries/logger.sql
+++ src/logger/database/queries/logger.sql
@@ -1,8 +1,10 @@
 USE logger;
 
 CREATE TABLE logs (
+  id INT NOT NULL AUTO_INCREMENT,
   occured TIMESTAMP,
   message VARCHAR(1024),
   event_type ENUM ('ERROR', 'INFO', 'WARNING', 'DEBUG') NOT NULL DEFAULT 'INFO',
-  service VARCHAR(256)
+  service VARCHAR(256),
+  PRIMARY KEY (id)
 );

77f4dd38afdb29b0a78f8d24b2b7d021f90c463f
eevee <emen22@student.bth.se>
2026-02-23 16:42:58 +0100

    Add Vue frontend with routing, login page, and initial Dockerfile init

---
+++ src/front_end/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import DefaultLayout from "@/layouts/DefaultLayout.vue"
+
+const route = useRoute()
+const isLoginPage = computed(() => route.path === '/')
+</script>
+
+<template>
+  <div v-if="isLoginPage">
+    <router-view />
+  </div>
+  <DefaultLayout v-else>
+    <router-view />
+  </DefaultLayout>
+</template>

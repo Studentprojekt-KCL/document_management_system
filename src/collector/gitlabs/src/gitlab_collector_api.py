6fb44ddbd026406ce0e0480f149fa4d1ccae3a31
jaen22BTH <145543571+jaen22BTH@users.noreply.github.com>
2026-02-25 06:04:21 -0500

    Feature/gitlabs mock (#150)
    * Initial stages for interface against gitlabs
    
    * Updated interfacer with functionallity to retrieve project files
    
    * Initial connector API parts
    
    * Removed hardcoded entrypoint
    
    * Finished demo of initial connector beta
    
    * Quick fix for import errors

---
+++ src/collector/gitlabs/src/gitlab_collector_api.py
@@ -0,0 +1,62 @@
+"""Copyright (c) 2026, Studentprojekt Knowit Cybersecurity and Law."""
+
+from typing import Any
+import argparse
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+from interfacer import GitLabs
+
+
+class API:
+    """Management class for Gitlabs connector API."""
+
+    app = FastAPI()
+
+    log_level: str | None = None
+
+    def __init__(self) -> None:
+        """Constructor."""
+        self.gitlabs_instance = GitLabs()
+        self.app.add_exception_handler(RequestValidationError, self.validation_exception_handler)
+        self.app.add_api_route("/files", self.files, methods=["GET"])
+        self.app.add_api_route("/file", self.file, methods=["GET"])
+
+    async def validation_exception_handler(self, _: Request, exc: Exception) -> JSONResponse:
+        """Overwrite FastAPI exception handeler."""
+        errors: dict
+        if isinstance(exc, RequestValidationError):
+            errors = {"detail": exc.errors(), "body": exc.body}
+        else:
+            errors = {"detail": str(exc)}
+        content: str | dict
+        if self.log_level == "debug":
+            content = jsonable_encoder(errors)
+        else:
+            content = "ERROR"
+        return JSONResponse(status_code=422, content=content)
+
+    async def files(self, subdata: str | None = None) -> Any:
+        """Endpoint returning a list of files available."""
+        return self.gitlabs_instance.pointers_to_all_files_to_index(subdata)
+
+    async def file(self, file_pointer: str, include_content: bool = True) -> Any:
+        """Endpoint for retrieving specific file."""
+        return self.gitlabs_instance.get_file(file_pointer, include_content)
+
+
+def run() -> None:
+    """Initiate FastAPI using Uvicorn."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dev", action="store_true")
+    args = parser.parse_args()
+
+    api = API()
+    if args.dev:
+        api.log_level = "debug"
+
+    uvicorn.run(api.app, host="0.0.0.0", log_level=api.log_level)

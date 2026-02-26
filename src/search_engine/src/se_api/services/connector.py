5efd8ecfe16e5f67fed22b89ee1c9b27b84f706e
Alexander Järnström <aljr21@student.bth.se>
2026-02-26 15:36:23 +0000

    added connector and search engine services

---
+++ src/search_engine/src/se_api/services/connector.py
@@ -0,0 +1,104 @@
+import base64
+from os import environ
+from datetime import datetime
+from time import strptime
+from logger import dms_error
+from requests import get, Response, exceptions
+
+from se_api.exceptions import SeAPIException
+from se_api.models import File, Metadata
+
+class Connector:
+    address: str | None
+    subdata: str | None
+
+    def __init__(self):
+        address = environ.get("SE_API_CONNECTOR_ADDRESS", None)
+        if address is None:
+            raise SeAPIException(f"SE_API_CONNECTOR_ADDRESS is not defined.")
+
+        self.address = address
+        self.subdata = None
+
+    def get_file_pointers(self) -> list[str]:
+        if self.address is None:
+            raise SeAPIException(f"SE_API_CONNECTOR_ADDRESS is not defined.")
+
+        params: dict[str, str] = {}
+
+        if self.subdata is not None:
+            params.update({"subdata": self.subdata})
+
+        response = get(f"{self.address}/files", params=params).json()
+
+        if not isinstance(response, dict):
+            return []
+
+        file_pointers: list[str] = []
+        pointers = response["file_pointers"]
+
+        if not isinstance(pointers, list):
+            raise SeAPIException(f"Expected results to be list[str].")
+
+        if not isinstance(response["subdata"], str):
+            raise SeAPIException(f"Expected subdata to be str.")
+
+        for pointer in pointers:
+            if isinstance(pointer, str):
+                file_pointers.append(pointer)
+
+        self.subdata = response["subdata"]
+
+        return file_pointers
+
+    def get_file(self, pointer: str) -> File | None:
+        try:
+            response = get(f"{self.address}/file", params={"file_pointer": pointer}).json()
+            if not isinstance(response["metadata"], dict):
+                raise SeAPIException("")
+
+            unique_pointer = response["metadata"]["unique_pointer"]
+            name = response["metadata"]["name"]
+            size = response["metadata"]["size"]
+            edited = response["metadata"]["last_edit_date"]
+            type = response["metadata"]["type"]
+            content = response["content"]
+
+            if not isinstance(unique_pointer, str): raise SeAPIException("")
+            if not isinstance(name, str): raise SeAPIException("")
+            if not isinstance(size, int): raise SeAPIException("")
+            if not isinstance(edited, str): raise SeAPIException("")
+            if not isinstance(type, str): raise SeAPIException("")
+            if not isinstance(content, str): raise SeAPIException("")
+
+            file: File = File(
+                content=content,
+                metadata=Metadata(
+                    unique_pointer=unique_pointer,
+                    name=name,
+                    size=size,
+                    edited=datetime.fromisoformat(edited),
+                    type=type
+                )
+            )
+
+            return file
+        except exceptions.InvalidJSONError as e:
+            dms_error(e.strerror if e.strerror is not None else "") 
+
+        return None
+
+    def get_files(self) -> list[File]:
+        pointers: list[str] = self.get_file_pointers()
+        files: list[File] = []
+        count: int = 0
+
+        for pointer in pointers:
+            file: File | None = self.get_file(pointer)
+            if file is not None:
+                files.append(file)
+            count += 1
+            print(f"{count}/{len(pointers)}")
+
+        return files
+
---
+++ src/search_engine/src/se_api/services/search_engine.py
@@ -0,0 +1,76 @@
+import base64
+from tantivy import Document, Index, IndexWriter, Query, SchemaBuilder, SearchResult, Searcher
+
+from se_api.exceptions import SeAPIException
+from se_api.models import File, Metadata
+
+class SearchEngine:
+    index: Index
+
+    def __init__(self) -> None:
+        schema_builder = SchemaBuilder()
+        _ = schema_builder.add_text_field("name", stored=True)
+        _ = schema_builder.add_text_field("unique_pointer", stored=True)
+        _ = schema_builder.add_text_field("edited", stored=True)
+        _ = schema_builder.add_text_field("type", stored=True)
+        _ = schema_builder.add_text_field("content", stored=True)
+        _ = schema_builder.add_integer_field("size",stored=True)
+        schema = schema_builder.build()
+
+        # Memory only
+        self.index = Index(schema)
+
+    def query_files(self, q: str, k: int = 50) -> list[str]:
+        searcher: Searcher = self.index.searcher()
+        query: Query = self.index.parse_query(q, ["name", "content", "unique_pointer"])
+        result: SearchResult = searcher.search(query, k)
+        pointers: list[str] = []
+        for (_, doc_id) in result.hits:
+            doc: Document = searcher.doc(doc_id)
+            unique_poinet = doc["unique_pointer"][0]
+
+            if not isinstance(unique_poinet, str):
+                raise SeAPIException("")
+
+            pointers.append(unique_poinet)
+
+        return pointers
+
+    def add_file(self, file: File) -> None:
+        writer: IndexWriter = self.index.writer()
+        content_byte = base64.b64decode(file.content)
+        content = content_byte.decode("utf-8")
+        _ = writer.add_document(Document(
+            name = file.metadata.name if file.metadata.name is not None else "",
+            unique_pointer = file.metadata.unique_pointer,
+            edited = file.metadata.edited.isoformat() if file.metadata.edited is not None else "",
+            type = file.metadata.type if file.metadata.type is not None else "",
+            size = file.metadata.size if file.metadata.size is not None else "",
+            content = content
+        ))
+        _ = writer.commit()
+        writer.wait_merging_threads()
+        self.index.reload()
+
+    def add_files(self, files: list[File]) -> None:
+        writer: IndexWriter = self.index.writer()
+        for file in files:
+            content_byte = base64.b64decode(file.content)
+            content = content_byte.decode("utf-8")
+            _ = writer.add_document(Document(
+                name = file.metadata.name if file.metadata.name is not None else "",
+                unique_pointer = file.metadata.unique_pointer,
+                edited = file.metadata.edited.isoformat() if file.metadata.edited is not None else "",
+                type = file.metadata.type if file.metadata.type is not None else "",
+                size = file.metadata.size if file.metadata.size is not None else "",
+                content = content
+            ))
+        _ = writer.commit()
+        writer.wait_merging_threads()
+        self.index.reload()
+
+
+
+        
+
+

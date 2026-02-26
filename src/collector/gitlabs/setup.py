81d20fbfa4af92432e6058f14c8cfb0feaa6edf1
jaen22BTH <jaen22@student.bth.se>
2026-02-26 11:52:30 -0500

    Updated global deps

--- src/collector/gitlabs/setup.py
+++ src/collector/gitlabs/setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup
+"""Copyright (c) 2026, Studentprojekt Knowit Cybersecurity and Law."""
+
 from pathlib import Path
+from setuptools import setup
 
 shared_lib_path = (Path(__file__).parent / "../../shared_functions").resolve().as_uri()
 

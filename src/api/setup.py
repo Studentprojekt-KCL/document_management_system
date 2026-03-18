0dc588a323284f3bd533fcf1a366b93fd8b81568
Marko <mamz22@student.bth.se>
2026-03-18 15:35:31 +0100

    ^^

--- src/api/setup.py
+++ src/api/setup.py
@@ -8,9 +8,8 @@ shared_lib_path = (Path(__file__).parent / "../shared_functions").resolve().as_u
 
 setup(
     install_requires=[
-        "fastapi>=0.128",
+        "fastapi[standard]>=0.128",
         "requests>=2.32",
-        "python-dotenv>=1.0",
         "uvicorn>=0.30",
         f"shared-functions @ {shared_lib_path}",
     ]

81912e938183d08aa2864f5d6931e3f1f28e938a
eevee <emen22@student.bth.se>
2026-03-20 14:21:26 +0100

    Comments, only a few files left :)

--- src/front_end/src/App.vue
+++ src/front_end/src/App.vue
@@ -1,21 +1,27 @@
 <script setup>
+/**
+ * The App.vue component serves as the main component for the frontend application.
+ * Handles global layout and synchronization of logout events across tabs.
+ */
+
 import { computed, onMounted, onUnmounted } from 'vue'
 import { useRoute } from 'vue-router'
 import MainLayout from '@/layouts/MainLayout.vue'
 import router from './router'
 
+/* Determine if the current route is a public or error page. */
 const route = useRoute()
 const isPublicOrErrorPage = computed(() => ['/', '/401', '/403', '/404'].includes(route.path))
 
+/* Synchronize logout across multiple tabs for storage events. */
 const syncLogout = (event) => {
   if (event.key === 'logout-event') {
-    console.log('Logout event detected from another tab.  cleaning up...')
     sessionStorage.clear()
-
     router.push('/')
   }
 }
 
+/* Event listeners for storage events to handle logout synchronization. */
 onMounted(() => {
   window.addEventListener('storage', syncLogout)
 })
@@ -26,9 +32,11 @@ onUnmounted(() => {
 </script>
 
 <template>
+  <!-- Layout based on the current state (error, not authenticated, or public page etc.). -->
   <div v-if="isPublicOrErrorPage">
     <router-view />
   </div>
+  <!-- For all other routes, use the main layout. -->
   <MainLayout v-else>
     <router-view />
   </MainLayout>

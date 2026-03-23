3c5ec0b52e1518da9d3d9867c2c617e9c68772c3
AlexanderJarnstromBTH <112385793+AlexanderJarnstromBTH@users.noreply.github.com>
2026-03-23 14:40:32 +0000

    Fix/logger/align with new (#232)
    * Added new environment variabbles for the logger and followed the new
    structure. #231
    
    * added mysql port variable to readme
    
    * Revert "added mysql port variable to readme"
    
    This reverts commit 2276c883e97b543f69e82d1fa6c9fe291c4dbc4e.
    
    * added description for database port variable #231
    
    * Happy tox #231

--- src/logger/api/src/log_api/api.py
+++ src/logger/api/src/log_api/api.py
@@ -1,8 +1,9 @@
 """Copyright (c) 2026, Studentprojekt Knowit Cybersecurity and Law"""
 
 import argparse
+from os import environ
 from typing import Any, Sequence
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import uvicorn
 from fastapi import FastAPI, Request
@@ -10,20 +11,63 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-from log_api.handlers import handel_get_logs, handel_add_log
+from log_api.database import Database
 from log_api.models import Log
+from dmis_logger import dms_error
 
 
 class API:
     """API object, holds all endpoints and configuration."""
 
-    app: FastAPI = FastAPI()
+    app: FastAPI
+    database: Database
 
     log_level: str | None = None
+    bind: str
+    port: int
 
     def __init__(self) -> None:
         """Constructor."""
+        port = environ.get("LOGGER_PORT")
+        bind = environ.get("LOGGER_BIND_ADDRESS")
+
+        if port is None:
+            dms_error("LOGGER_PORT is not defined.")
+            return
+        if bind is None:
+            dms_error("LOGGER_BIND_ADDRESS is not defined.")
+            return
+
+        if not port.isdigit():
+            dms_error("Expected LOGGER_PORT to be an integer.")
+            return
+        if int(port) <= 0 or int(port) >= 65535:
+            dms_error("Expected LOGGER_PORT to be between 0 and 65545.")
+            return
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--dev", action="store_true")
+        args = parser.parse_args()
+
+        if args.dev:
+            self.log_level = "debug"
+
+        self.bind = bind
+        self.port = int(port)
+
+        self.app = FastAPI()
+
+        self.app.add_api_route("/logs", self.get_logs, methods=["GET"])
+        self.app.add_api_route("/logs", self.get_logs, methods=["POST"])
+        self.app.add_api_route("/health", self.get_logs, methods=["GET"])
+
         self.app.add_exception_handler(RequestValidationError, self.validation_exception_handler)
+
+        self.database = Database()
+
+    def start(self) -> None:
+        """Start the api."""
+        uvicorn.run(self.app, host=self.bind, log_level=self.log_level)
 
     async def validation_exception_handler(self, _: Request, exc: Exception) -> JSONResponse:
         """Overwrite FastAPI exception handeler."""
@@ -39,33 +83,25 @@ class API:
             content = "ERROR"
         return JSONResponse(status_code=422, content=content)
 
-    @staticmethod
-    @app.get("/logs")
-    async def get_logs(start: datetime | None = None, end: datetime | None = None) -> list[Log] | None:
+    async def get_logs(self, start: datetime | None = None, end: datetime | None = None) -> list[Log] | None:
         """Get logs, either returns a list or None"""
-        return handel_get_logs(start, end)
+        if start is None:
+            start = datetime.now() + timedelta(hours=-1)
+        if end is None:
+            end = datetime.now()
 
-    @staticmethod
-    @app.post("/logs")
-    async def add_log(log: Log) -> Log:
+        return self.database.database_get_logs(start, end)
+
+    async def add_log(self, log: Log) -> Log:
         """Add a log to the database, returns the Log."""
-        return handel_add_log(log)
+        return self.database.database_add_log(log)
 
-    @staticmethod
-    @app.get("/health")
-    async def check_health() -> JSONResponse:
+    async def check_health(self) -> JSONResponse:
         """Respond to health check"""
         return JSONResponse(status_code=200, content={"msg": "healthy"})
 
 
 def run() -> None:
     """Initiate FastAPI using Uvicorn."""
-    parser = argparse.ArgumentParser()
-    _ = parser.add_argument("--dev", action="store_true")
-    args = parser.parse_args()
-
     api = API()
-    if args.dev:
-        api.log_level = "debug"
-
-    uvicorn.run(api.app, host="0.0.0.0", log_level=api.log_level)
+    api.start()
--- src/logger/api/src/log_api/database.py
+++ src/logger/api/src/log_api/database.py
@@ -2,6 +2,7 @@
 
 import os
 from datetime import datetime
+from dmis_logger import dms_error
 import mysql.connector
 from mysql.connector.abstracts import MySQLConnectionAbstract
 from mysql.connector.pooling import PooledMySQLConnection
@@ -9,65 +10,88 @@ from mysql.connector.types import RowItemType, RowType
 from log_api.models import Log
 
 
-def connect() -> PooledMySQLConnection | MySQLConnectionAbstract:
-    """Return database connection."""
+class Database:
+    """Service class for the database."""
 
-    host = os.environ.get("LOGGER_DB_HOST")
-    user = os.environ.get("LOGGER_DB_USER")
-    password = os.environ.get("LOGGER_DB_PASS")
-    database = os.environ.get("LOGGER_DB_DATABASE")
+    host: str
+    user: str
+    password: str
+    database: str
 
-    return mysql.connector.connect(host=host, user=user, database=database, password=password)
+    def __init__(self) -> None:
+        host = os.environ.get("LOGGER_DB_HOST")
+        user = os.environ.get("LOGGER_DB_USER")
+        password = os.environ.get("LOGGER_DB_PASS")
+        database = os.environ.get("LOGGER_DB_DATABASE")
 
+        if host is None:
+            dms_error("LOGGER_DB_HOST is undefined.")
+            return
+        if user is None:
+            dms_error("LOGGER_DB_USER is undefined.")
+            return
+        if password is None:
+            dms_error("LOGGER_DB_PASS is undefined.")
+            return
+        if database is None:
+            dms_error("LOGGER_DB_DATABASE is undefined.")
+            return
 
-def extract_row_data(row: RowType | dict[str, RowItemType]) -> Log:
-    """Extract each values from a row validating the type.
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
 
-    Keyword arguments:
-    row -- Row from the database.
-    """
+    def connect(self) -> PooledMySQLConnection | MySQLConnectionAbstract:
+        """Return database connection."""
+        return mysql.connector.connect(host=self.host, user=self.user, database=self.database, password=self.password)
 
-    if not isinstance(row, tuple):
-        raise TypeError("Row is of wrong type.")
+    def extract_row_data(self, row: RowType | dict[str, RowItemType]) -> Log:
+        """Extract each values from a row validating the type.
 
-    log_id: int | None = row[0] if isinstance(row[0], int) else None
-    occured: datetime | None = row[1] if isinstance(row[1], datetime) else None
-    message: str | None = row[2] if isinstance(row[2], str) else None
-    event_type: str | None = row[3] if isinstance(row[3], str) else None
-    service: str | None = row[4] if isinstance(row[4], str) else None
+        Keyword arguments:
+        row -- Row from the database.
+        """
 
-    if log_id is None:
-        raise TypeError("Id is of wrong type.")
-    if occured is None:
-        raise TypeError("Occured is of wrong type.")
-    if message is None:
-        raise TypeError("Message is of wrong type.")
-    if event_type is None:
-        raise TypeError("Event Type is of wrong type.")
-    if service is None:
-        raise TypeError("Service is of wrong type.")
+        if not isinstance(row, tuple):
+            raise TypeError("Row is of wrong type.")
 
-    return Log(id=log_id, occured=occured, message=message, event_type=event_type, service=service)
+        log_id: int | None = row[0] if isinstance(row[0], int) else None
+        occured: datetime | None = row[1] if isinstance(row[1], datetime) else None
+        message: str | None = row[2] if isinstance(row[2], str) else None
+        event_type: str | None = row[3] if isinstance(row[3], str) else None
+        service: str | None = row[4] if isinstance(row[4], str) else None
 
+        if log_id is None:
+            raise TypeError("Id is of wrong type.")
+        if occured is None:
+            raise TypeError("Occured is of wrong type.")
+        if message is None:
+            raise TypeError("Message is of wrong type.")
+        if event_type is None:
+            raise TypeError("Event Type is of wrong type.")
+        if service is None:
+            raise TypeError("Service is of wrong type.")
 
-def database_get_logs(start: datetime, end: datetime) -> list[Log]:
-    """Grab all logs."""
+        return Log(id=log_id, occured=occured, message=message, event_type=event_type, service=service)
 
-    db = connect()
-    cursor = db.cursor()
-    query: str = "SELECT * FROM logs WHERE occured BETWEEN %s AND %s"
-    _ = cursor.execute(query, (start, end))
-    result = cursor.fetchall()
+    def database_get_logs(self, start: datetime, end: datetime) -> list[Log]:
+        """Grab all logs."""
 
-    return [extract_row_data(row) for row in result]
+        db = self.connect()
+        cursor = db.cursor()
+        query: str = "SELECT * FROM logs WHERE occured BETWEEN %s AND %s"
+        _ = cursor.execute(query, (start, end))
+        result = cursor.fetchall()
 
+        return [self.extract_row_data(row) for row in result]
 
-def database_add_log(log: Log) -> Log:
-    """Add new log to database and return a Log."""
+    def database_add_log(self, log: Log) -> Log:
+        """Add new log to database and return a Log."""
 
-    db = connect()
-    cursor = db.cursor()
-    sql = "INSERT INTO logs (occured, message, event_type, service) VALUES (%s, %s, %s, %s)"
-    _ = cursor.execute(sql, log.to_values())
-    _ = db.commit()
-    return log
+        db = self.connect()
+        cursor = db.cursor()
+        sql = "INSERT INTO logs (occured, message, event_type, service) VALUES (%s, %s, %s, %s)"
+        _ = cursor.execute(sql, log.to_values())
+        _ = db.commit()
+        return log

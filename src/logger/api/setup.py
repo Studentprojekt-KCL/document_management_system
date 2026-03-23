5db1ffea3f7dda39379a28ef525e742886d407ff
Alexander Järnström <aljr21@student.bth.se>
2026-03-23 14:10:56 +0000

    Added new environment variabbles for the logger and followed the new structure. #231

---
+++ src/logger/api/setup.py
@@ -0,0 +1,14 @@
+"""Copyright (c) 2026, Studentprojekt Knowit Cybersecurity and Law."""
+
+from pathlib import Path
+from setuptools import setup
+
+shared_lib_path = (Path(__file__).parent / "../../shared_functions").resolve().as_uri()
+
+setup(
+    install_requires=[
+        "fastapi[standard]>=0.128",
+        "mysql-connector-python >= 9.5",
+        f"shared-functions @ {shared_lib_path}",
+    ]
+)

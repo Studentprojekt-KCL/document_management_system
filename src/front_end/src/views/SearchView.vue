2ea8edbe6ee7d269d3008b4b4b8eaced33b198bb
osoh22 <osoh22@student.bth.se>
2026-04-13 12:35:50 +0200

    fixed search request formatting for mainAPI call.

--- src/front_end/src/views/SearchView.vue
+++ src/front_end/src/views/SearchView.vue
@@ -57,8 +57,7 @@ const handleSearch = async (query) => {
   try {
     const res = await fetch(`${API_BASE_URL}/search?query=${encodeURIComponent(query)}`, {
       headers: {
-        Authorization: `Bearer ${access_token}`,
-        'Content-Type': 'application/json'
+        Authorization: `Bearer ${access_token}`
       }
     })
 

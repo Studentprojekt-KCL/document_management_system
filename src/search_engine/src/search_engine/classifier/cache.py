59daee8a8e90223b59464b3d23a6516f64af6aa3
Alexander Järnström <aljr21@student.bth.se>
2026-04-14 06:19:10 +0000

    Revert "slightly better constructor"
    This reverts commit 18e026e9ab24e72355cf3e83f5c61655ac4ea73b.

--- src/search_engine/src/search_engine/classifier/cache.py
+++ src/search_engine/src/search_engine/classifier/cache.py
@@ -1,7 +1,6 @@
 """Copyright (c) 2026, Studentprojekt Knowit Cybersecurity and Law"""
 
 import json
-from os import path
 
 from dmis_logger import dms_error, dms_info
 from initialisation_tools import read_env_variable
@@ -23,32 +22,26 @@ class Cache:
 
     def __init__(self) -> None:
         """Constructor"""
-        self.cache_directory: str = read_env_variable("SE_API_CACHE_DIRECTORY")
-        self.cache_file: str = f"{self.cache_directory.rstrip('/')}/{Cache.CACHE_FILE}"
-        self.cache = {}
+        cache_directory: str = read_env_variable("SE_API_CACHE_DIRECTORY")
+        cache_file: str = f"{cache_directory.rstrip('/')}/{ClassifierCache.CACHE_FILE}"
 
-        if not path.isdir(self.cache_directory):
-            dms_error(f"{self.cache_directory} is not a directory.")
-        elif path.isfile(self.cache_file):
+        try:
+            with open(cache_file, encoding="utf=8") as f:
+                dms_info(f"Found {cache_file}.")
+                cache: dict = json.loads(f.read())
+                self.cache = cache
+        except OSError:
             try:
-                with open(self.cache_file, encoding="utf-8") as f:
-                    for line in f.readlines():
-                        classification: dict[str, str] = json.loads(line.strip().replace("'", '"'))
-                        self.cache.update(classification)
-                dms_info(f"Found file: {self.cache_file}.")
+                dms_info(f"Creating {cache_file}.")
+                with open(cache_file, "x", encoding="utf=8"):
+                    self.cache = {}
             except OSError:
-                dms_error(f"Failed reading file: {self.cache_file}.")
-            except json.JSONDecodeError as e:
-                print(e.msg)
-                dms_error(f"Failed parsing file: {self.cache_file}.")
-        elif path.exists(self.cache_file):
-            dms_error(f"{self.cache_file} is not a file.")
-        else:
-            try:
-                with open(self.cache_file, "x", encoding="utf-8") as f:
-                    dms_info(f"Created file: {self.cache_file}.")
-            except OSError:
-                dms_error(f"Failed reading file: {self.cache_file}.")
+                dms_error(f"Failed to create {cache_file}.")
+                return
+        except json.JSONDecodeError:
+            self.cache = {}
+
+        self.cache_file = cache_file
 
         self.writer = Writer(self.cache_file)
         self.writer.start()

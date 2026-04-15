5a86edbb13571e82892c1f6a9c32d7304d59c50f
muaathaw <muaw22@student.bth.se>
2026-04-15 19:30:50 +0200

    edit API call

--- src/front_end/src/components/SearchPreviewDrawer.vue
+++ src/front_end/src/components/SearchPreviewDrawer.vue
@@ -45,7 +45,7 @@ const { previewTitle, previewType, sourceSystem, previewCreatedAt, previewSize, 
 const { aiSummaryHtml, summaryError, isGeneratingSummary, generateAISummary } = useAISummary(props)
 
 /* API */
-const API_BASE_URL = import.meta.env.API_BASE_URL?.replace(/\/$/, '') ?? ''
+const API_BASE_URL = window.__ENV__.API_BASE_URL?.replace(/\/$/, '') ?? ''
 
 const isEditingClassification = ref(false)
 const classificationEditorRef = ref(null)

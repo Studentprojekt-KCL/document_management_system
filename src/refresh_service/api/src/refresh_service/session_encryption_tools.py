5d51be0897d6896b1b7d83b11e0c9a72f2946b54
jaen22BTH <jaen22@student.bth.se>
2026-04-24 12:23:57 -0400

    Optimized encryption tools for db insertions

--- src/refresh_service/api/src/refresh_service/session_encryption_tools.py
+++ src/refresh_service/api/src/refresh_service/session_encryption_tools.py
@@ -9,10 +9,15 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 
 class SessionEncryption:
+    """Tools for encrypting and decrypting session variables."""
 
-    suite: Fernet
+    def __init__(self, secret_key):
+        """Constructor."""
+        self.suite = self._generate_suite(secret_key)
 
-    def __init__(self, secret_key: str):
+    @staticmethod
+    def _generate_suite(secret_key) -> Fernet:
+        """Generate Fernet suite with secret key."""
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
@@ -21,12 +26,15 @@ class SessionEncryption:
         )
 
         encoded_key = base64.urlsafe_b64encode(kdf.derive(secret_key.encode()))
-        self.suite = Fernet(encoded_key)
+        return Fernet(encoded_key)
 
-    def encrypt_session_vars(self, session_vars: dict):
+    def encrypt_session_vars(self, session_vars: dict) -> str:
+        """Cast given dictionary into str and return encrypted string."""
         session_str = json.dumps(session_vars)
         raw_string = session_str.encode()
-        return self.suite.encrypt(raw_string)
+        return self.suite.encrypt(raw_string).decode('utf-8')
 
-    def decrypt_session_vars(self, encrypted_bytes: bytes):
-        return self.suite.decrypt(encrypted_bytes).decode()
+    def decrypt_session_vars(self, encrypted_str: str) -> dict:
+        """Decrypt session variables."""
+        encrypted_bytes = bytes(encrypted_str, "utf-8")
+        return json.loads(self.suite.decrypt(encrypted_bytes).decode())

a5a2f35fc7cc461e3b007629370a4090e3b77808
jaen22BTH <jaen22@student.bth.se>
2026-04-24 11:42:04 -0400

    Working DB integrations

--- src/refresh_service/api/src/refresh_service/database.py
+++ src/refresh_service/api/src/refresh_service/database.py
@@ -3,6 +3,11 @@
 from mysql import connector
 from shared_functions.initialisation_tools import read_env_variable
 
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
+
+import time
+
 class Database:
     """Service class for the database."""
 
@@ -16,3 +21,51 @@ class Database:
         self.user = read_env_variable("REFSERVICE_DB_USER")
         self.password = read_env_variable("REFSERVICE_DB_PASSW")
         self.database = read_env_variable("REFSERVICE_DB_DATABASE")
+
+    def connect(self) -> PooledMySQLConnection | MySQLConnectionAbstract:
+        """Return database connection."""
+        return connector.connect(host=self.host, user=self.user, database=self.database, password=self.password)
+
+    def get_session_token(self, user: str, service: str):
+        """Retrive session token from database.
+
+        Args:
+        ----
+            user: Users sub UUID.
+            service: Name of service token authenticates against.
+        """
+        db = self.connect()
+        cursor = db.cursor()
+        query: str = "SELECT token FROM user_sessions WHERE user_id = %s AND service = %s"
+        _ = cursor.execute(query, (user, service))
+        result = cursor.fetchall()
+
+        if len(result) != 1:
+            return []
+
+        return result[0][0]
+
+    def insert_session_token(self, user: str, service: str, enc_obj: str, expiry_time: int):
+        """Insert encrypted user tokens into database.
+
+        Args:
+        ----
+            user: Users sub UUID.
+            service: Name of service token authenticates against.
+            enc_obj: Encrypted session token.
+            expiry_time: Number of seconds until token expires.
+        """
+
+        timestamp = int(time.time()) + expiry_time
+
+        db = self.connect()
+        cursor = db.cursor()
+        sql = """
+        INSERT INTO user_sessions (user_id, service, token, expiry_time)
+        VALUES (%s, %s, %s, FROM_UNIXTIME(%s))
+        ON DUPLICATE KEY UPDATE
+            token = %s,
+            expiry_time = FROM_UNIXTIME(%s)
+        """
+        cursor.execute(sql, (user, service, enc_obj, timestamp, enc_obj, timestamp))
+        db.commit()

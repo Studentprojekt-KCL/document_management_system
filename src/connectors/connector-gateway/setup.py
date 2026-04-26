aec6316a21ac8a312186f86d75107e5e722ab4cf
Jesper Lennehag <jesper@lennehag.com>
2026-04-27 01:54:31 +0200

    Changed docker file, tox passes

--- src/connectors/connector-gateway/setup.py
+++ src/connectors/connector-gateway/setup.py
@@ -1,12 +1,8 @@
+"""setup file for connector-gateway module"""
+
 from pathlib import Path
 from setuptools import setup
 
 shared_lib_path = (Path(__file__).parent / "../../shared_functions").resolve().as_uri()
 
-setup(
-    install_requires=[
-        "fastapi[standard]>=0.128",
-        "httpx>=0.28.1",
-        f"shared-functions @ {shared_lib_path}"
-    ]
-)
+setup(install_requires=["fastapi[standard]>=0.128", "httpx>=0.28.1", f"shared-functions @ {shared_lib_path}"])
--- src/connectors/connector-gateway/src/connector_gateway/connector_client.py
+++ src/connectors/connector-gateway/src/connector_gateway/connector_client.py
@@ -1,68 +1,56 @@
+"""Module for interacting with connectors to source sysetms"""
+
 import asyncio
-from os import environ
-from typing import Any
+import json
 import httpx
 
-# from shared_functions.dmis_logger import dms_error, dms_warning
 
 class ConnectorClient:
-    TIMEOUT: int = 120
+    """
+    Class for interacing with connectors to source systems
+    """
 
-    source_systems: list[dict]  # List of dicts of subsytems
-    source_systems_names: list[dict] | None
-    subdata: str | None
+    timeout: int
+    source_systems: list[dict]
 
-    get_files_urls: list[str]  # URls to /get_files endpoints
+    def __init__(self, config_path: str, timeout: int) -> None:
 
-    def __init__(self) -> None:
-        self.source_systems = [
-            {
-                "name": "Gitlab",
-                "connector_url": "http://connector-gitlab.dev.dms-lookup.com",
-                "source_system_url": "https://gitlab.dms-lookup.com",
-            },
-            {
-                "name": "Github",
-                "connector_url": "http://connector-github.dev.dms-lookup.com",
-                "source_system_url": "https://github.com",
-            },
-            {
-                "name": "Confluence",
-                "connector_url": "http://connector-confluence.dev.dms-lookup.com",
-                "source_system_url": "https://confluence.com",
-            },
-        ]
-        self.subdata = None
-        if self.source_systems == [{}]:
-            return
+        self.timeout = timeout
+        self.source_systems = self._load_source_systems_from_file(config_path)
+
         for system in self.source_systems:
             system["connector_url"] = system["connector_url"].rstrip("/")
             system["source_system_url"] = system["source_system_url"].rstrip("/")
 
-    async def fetch_files_metadata(
-        self, pointers: list[str], include_content, include_last_edit_date
-    ) -> list[dict]:
-        if pointers == []:
+    def _load_source_systems_from_file(self, path: str) -> list:
+        """Reads config file and loads it into the program"""
+        with open(path, encoding="utf-8") as config_file:
+            config = json.load(config_file)
+        return config
+
+    async def fetch_files_metadata(self, pointers: list[str], include_content: bool, include_last_edit_date: bool) -> list[dict]:
+        """Returns meta data about files pointed to by pointers"""
+
+        if not pointers:
             return []
+
         sorted_pointers: list[list] = self._sort_pointers(pointers)
-        response = await self._get_file_from_pointer(
-            sorted_pointers, include_content, include_last_edit_date
-        )
-        return response
+
+        meta_data: list = await self._get_file_from_pointer(sorted_pointers, include_content, include_last_edit_date)
+        return meta_data
 
     def _sort_pointers(self, pointers: list[str]) -> list[list]:
-        """Sorts pointers according to host part pointer URL eg Gitlab, Github etc"""
-        if pointers == []:
-            return []
-        sorted_pointers = []
+        """Sorts pointers according to host part of pointer URL eg Gitlab, Github etc"""
+
+        sorted_pointers: list[list] = []
 
         for pointer in pointers:
-            if sorted_pointers == []:
+            if not sorted_pointers:
                 sorted_pointers.append([pointer])
                 continue
 
             host = pointer.split("//")[-1].split("/")[0]
-            for grouped_pointers in sorted_pointers:
+            for grouped_pointers in sorted_pointers.copy():
                 if host in grouped_pointers[0]:
                     grouped_pointers.append(pointer)
                     continue
@@ -70,25 +58,24 @@ class ConnectorClient:
 
         return sorted_pointers
 
-    def _slice_url_to_host_and_proto(self, url):
+    def _slice_url_to_host_and_proto(self, url: str) -> str:
+        """Returns a URL proto:://<host>/path -> proto://<host>"""
         scheme, rest = url.split("://", 1)
         host = rest.split("/", 1)[0]
         return f"{scheme}://{host}"
 
     async def _get_file_from_pointer(
-        self, sorted_pointers: list[list], include_content, include_last_edit_date
-    ) -> Any | None:
+        self, sorted_pointers: list[list], include_content: bool, include_last_edit_date: bool
+    ) -> list:
+        """makes http requests to all source systems to gather meta data from pointers"""
+
         # Get hostnames for connectors according to pointers
         source_system_host_list = []
         for grouped_pointers in sorted_pointers:
             for system in self.source_systems:
-                if (
-                    self._slice_url_to_host_and_proto(grouped_pointers[0])
-                    in system["source_system_url"]
-                ):
+                if self._slice_url_to_host_and_proto(grouped_pointers[0]) in system["source_system_url"]:
                     source_system_host_list.append(system["connector_url"])
-                    continue
-                return  # return if source url could not be found
+                    break
 
         # Send requests to downstream connectors
         async with httpx.AsyncClient() as client:
@@ -100,7 +87,7 @@ class ConnectorClient:
                         ("include_last_edit_date", include_last_edit_date),
                     ],
                     json={"file_pointers": grouped_pointers},
-                    timeout=ConnectorClient.TIMEOUT,
+                    timeout=self.timeout,
                 )
                 for grouped_pointers in sorted_pointers
             ]
@@ -108,6 +95,7 @@ class ConnectorClient:
             return [item for r in responses for item in r.json()]
 
     def fetch_start_of_streams(self) -> list[str]:
+        """returns URL to connector for stream proto://<connector-host>/stream_files_to_index"""
         stream_urls: list[str] = []
         for source_system in self.source_systems:
             proto_host_url = source_system["connector_url"]
@@ -116,6 +104,7 @@ class ConnectorClient:
         return stream_urls
 
     def get_source_system_names(self) -> list[str]:
+        """Returns names of source systems according to config file"""
         names_of_source_systems: list[str] = []
         for source_system in self.source_systems:
             names_of_source_systems.append(source_system["name"])

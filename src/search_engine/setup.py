67b934374ad91c5d3099914456422d57ba4aabac
Alexander Järnström <aljr21@student.bth.se>
2026-05-02 19:26:53 +0000

    added back versions

--- src/search_engine/setup.py
+++ src/search_engine/setup.py
@@ -7,10 +7,10 @@ shared_lib_path = (Path(__file__).parent / "../shared_functions").resolve().as_u
 
 setup(
     install_requires=[
-        "fastapi[standard]",
-        "tantivy",
-        "httpx",
-        "markitdown[all]",
+        "fastapi[standard]>=0.128",
+        "tantivy>=0.25",
+        "httpx>=0.28",
+        "markitdown[pptx, docx, xlsx, xls, pdf]>=0.1.5",
         f"shared-functions @ {shared_lib_path}",
     ]
 )

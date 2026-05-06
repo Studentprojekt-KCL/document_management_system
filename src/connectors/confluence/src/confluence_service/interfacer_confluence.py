1b6a866fc025cfd86be1d87bb317bede7689ed5d
jaen22BTH <jaen22@student.bth.se>
2026-05-06 06:51:05 -0400

    Solved clickable URL #509

--- src/connectors/confluence/src/confluence_service/interfacer_confluence.py
+++ src/connectors/confluence/src/confluence_service/interfacer_confluence.py
@@ -319,7 +319,7 @@ class ConfluenceInterfacer:
             "name": title,
             "last_edit_date": when,
             "type": SOURCE_FILE,
-            "clickable_url": (urljoin(self.address + "/", webui.lstrip("/")) if isinstance(webui, str) else None),
+            "clickable_url": (urljoin(self.address + "/wiki/", webui.lstrip("/")) if isinstance(webui, str) else None),
         }
         if include_content:
             out_structure["content"] = base64.b64encode(text.encode("utf-8")).decode("utf-8")

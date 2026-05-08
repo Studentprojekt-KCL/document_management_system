ea740c8a7b6790c24434289e8c9ebe70975731b6
jaen22BTH <jaen22@student.bth.se>
2026-05-08 14:18:06 -0400

    Working authentication throught gateway

--- src/connectors/connector-gateway/src/connector_gateway/refreshservice_client.py
+++ src/connectors/connector-gateway/src/connector_gateway/refreshservice_client.py
@@ -1,5 +1,6 @@
 """Methods for interacting with refresh service."""
 
+import json
 import httpx
 
 from shared_functions.dmis_logger import dms_warning
@@ -18,16 +19,23 @@ class RefreshServiceClient:
         self.timeout = timeout
         self.http_client = httpx.AsyncClient()
 
-    async def send_request(
-        self, end_point: str, params: dict | None = None, headers: dict | None = None, body: dict | None = None
+    async def send_post_request(
+        self, end_point: str, params: dict | None = None, headers: dict | None = None, body: dict | list | None = None
     ) -> list | dict:
-        """Send request to refresh service."""
+        """Send POST request to refresh service."""
         try:
-            await self.http_client.post(
-                f"{self.service_url}{end_point}", params=params, headers=headers, json=body, timeout=self.timeout
+            response = await self.http_client.post(
+                f"{self.service_url.rstrip('/')}/{end_point.lstrip('/')}",
+                params=params,
+                headers=headers,
+                json=body,
+                timeout=self.timeout,
             )
+            return response.json()
+        except json.JSONDecodeError:
+            dms_warning("Unauthorized request to request service.")
         except httpx.TimeoutException:
             dms_warning("Request timed out")
         except httpx.HTTPError:
             dms_warning("Failed to connect to connector.")
-        return []
+        return {}
